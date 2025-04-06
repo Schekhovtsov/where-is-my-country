b63eec47dae32d15c3761aa5daf7986e3b700470
Sergey Shehovcov <s.shehovcov@gk-adept.ru>
2025-04-06 08:52:20 +0300

    feat: input with search and debounce

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss',
 })
-export class SearchComponent {}
+export class SearchComponent {
+  @Input() search = '';
+  @Output() changeSearchEvent = new EventEmitter<string>();
+
+  private timeout: any;
+
+  onSearchChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+
+    clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      this.changeSearchEvent.emit(value);
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.timeout);
+  }
+}
